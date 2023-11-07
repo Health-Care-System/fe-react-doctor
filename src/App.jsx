cb0a8b82b653b32bcef3acb9844f8c0c18e87bd3
nandorfn <firnandorfn@gmail.com>
2023-11-07 17:27:17 +0700

    add: textarea & update: select option

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,35 @@
 import { Button } from "./components/ui/Button"
-import { Select } from "./components/ui/Form/Select"
+import { Select, Textarea } from "./components/ui/Form"
 
 function App() {
   const options = [
-    {value: 'aku', label: 'Aku'},
-    {value: 'aku2', label: 'Aku2'}
+    { value: 'aku', label: 'Aku' },
+    { value: 'aku2', label: 'Aku2' }
   ]
 
   return (
     <>
-      <Select
-        options={options}
-      />
+      <div className="container d-flex gap-3 flex-column">
+      
+        <label>
+          Select Option
+          <Select options={options} />
+        </label>
+        
+        <label>
+          Textarea
+          <Textarea />
+        </label>
+        
+        <Button
+          className="btn-success my-2"
+          onClick={() => { }}
+          id="button"
+        >
+          Success Button
+        </Button>
+      </div>
 
-      <Button className="btn-success my-2" onClick={() => {}} id="button" >Success Button</Button>
     </>
   )
 }
