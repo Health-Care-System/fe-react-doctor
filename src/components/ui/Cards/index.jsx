cf9adf0c9208ffbdfd81cf7501afd892bb5429ea
nandorfn <firnandorfn@gmail.com>
2023-11-08 22:30:54 +0700

    add medicalprescriptioncard

--- src/components/ui/Cards/index.jsx
+++ src/components/ui/Cards/index.jsx
@@ -41,28 +41,31 @@ export const ArticleCard = ({ title, content, date }) => {
 }
 
 
-export const MedicalPrescriptionCard = () => {
+export const MedicalPrescriptionCard = ({ data }) => {
   return (
     <div className="card shadow rounded-4 border-0 medical-card">
-      <div className="card-body">
-        <figure className="d-inline-flex gap-2">
+      <div className="card-body px-0">
+        <figure className="d-flex px-2 border-bottom border-black pb-2 gap-2">
           <img src={drugIcon} alt="Drug" />
-          <h5 className="card-title fw-bold">Resep Digital</h5>
+          <h5 className="mt-2">Resep Digital</h5>
         </figure>
-        <section className=" d-flex justify-content-between card-subtitle mb-2 text-body-secondary fs-4">
+        <section className="d-flex justify-content-between text-body-secondary fs-4 px-2">
           <p>Nama Produk</p>
           <p>Jumlah</p>
         </section>
-        <p className="card-text">
-          Some quick example text to build on the card title and make up the bulk of
-          the cards content.
-        </p>
-        <a href="#" className="card-link">
-          Card link
-        </a>
-        <a href="#" className="card-link">
-          Another link
-        </a>
+        <ul className="list-group">
+          {data?.map((item, index) => (
+            <li key={index} className="list-group-item border-0 d-flex justify-content-between align-items-center p-0">
+              <p>{item.name}</p>
+              <p>{`${item.quantity} per Stripe`}</p>
+            </li>
+          ))
+          }
+        </ul>
+
+        <div className="d-flex justify-content-center">
+          <Button className={'btn-primary text-white w-75'}>Beli Obat</Button>
+        </div>
       </div>
     </div>
 
