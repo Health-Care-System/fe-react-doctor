a9b5b56313666e3a83c3bdedf5d68bbddd85e12a
Stephen Meycelino Fransisco <stephenmeycelinowork88@gmail.com>
2023-11-08 22:46:22 +0700

    add: component ui bubble for chat

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-import { Button } from "./components/ui/Button"
-import { ArticleCard } from "./components/ui/Card/ArticleCard"
-import { PatientTableRow } from "./components/ui/Cards/PatientTableRow";
-import { Select, Textarea } from "./components/ui/Form"
+import { Bubble } from "./components/ui/Bubble";
+import { Button } from "./components/ui/Button";
+import { ArticleCard } from "./components/ui/Cards";
+import { PatientTableRow } from "./components/ui/Cards";
+import { Select, Textarea } from "./components/ui/Form";
 
 function App() {
   const options = [
-    { value: 'aku', label: 'Aku' },
-    { value: 'aku2', label: 'Aku2' }
-  ]
+    { value: "aku", label: "Aku" },
+    { value: "aku2", label: "Aku2" },
+  ];
 
   const data = {
     name: "Joshua Kristin",
@@ -16,13 +17,12 @@ function App() {
     disease: "Demam",
     date: "17 Okt 23",
     status: "Recover",
-    image: "https://placehold.co/600x400.png"
+    image: "https://placehold.co/600x400.png",
   };
   return (
     <>
       <div className="container d-flex gap-3 flex-column">
-
-        <ArticleCard title={"test"} content={"test"} date={'test'}/>      
+        <ArticleCard title={"test"} content={"test"} date={"test"} />
         <label>
           Select Option
           <Select options={options} />
@@ -33,16 +33,14 @@ function App() {
           <Textarea />
         </label>
 
-        <Button
-          className="btn-info my-2"
-          onClick={() => { }}
-          id="button"
-        >
+        <Button className="btn-info my-2" onClick={() => {}} id="button">
           Success Button
         </Button>
 
         <PatientTableRow data={data} />
 
+        <Bubble className="bg-transparent rounded-top-3 rounded-end-3" text="Hallo dok, saya punya keluhan diare udah 10 kali bolak-balik kamar mandi, mual-mual, dan pusing sekali. Saya sudah coba minum norit tapi ga pengaruh sama sekali, kira-kira kenapa ya dok? tks" />
+        <Bubble className="bg-success-subtle rounded-top-3 rounded-start-3 align-self-end " text="Hallo dok, saya punya keluhan diare udah 10 kali bolak-balik kamar mandi, mual-mual, dan pusing sekali. Saya sudah coba minum norit tapi ga pengaruh sama sekali, kira-kira kenapa ya dok? tks" />
       </div>
     </>
   );
