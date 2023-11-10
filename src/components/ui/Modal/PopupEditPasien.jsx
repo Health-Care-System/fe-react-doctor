325903c67a46b6ead17272d7c1a156704bb327e7
Stephen Meycelino Fransisco <stephenmeycelinowork88@gmail.com>
2023-11-10 21:52:00 +0700

    fix: component ui modals patient looping data

--- src/components/ui/Modal/PopupEditPasien.jsx
+++ src/components/ui/Modal/PopupEditPasien.jsx
@@ -21,7 +21,7 @@ import closeMenu from "../../../assets/icon/close.svg";
 import { Button } from "../Button";
 import styles from "./Modal.module.css";
 import avatarIcon from "../../../assets/icon/avatar.svg";
-import { Select } from "../Form";
+import { Input, Select } from "../Form";
 
 export const PopupEditPasien = ({ closeModal, patientData }) => {
   const { name, weight, gender, consultationDate } = patientData;
@@ -35,58 +35,53 @@ export const PopupEditPasien = ({ closeModal, patientData }) => {
       <div className={`mx-2 ${styles.modalContainer}`}>
         <div className="d-flex justify-content-between align-items-center border-bottom pb-2 pb-lg-3 container-fluid ">
           <h5 className={styles.font__size}>Edit</h5>
-          <div className="btn">
+          <Button className="btn">
             <img
               src={closeMenu}
               alt="closeMenu"
               className="w-75"
               onClick={() => closeModal(false)}
             />
-          </div>
+          </Button>
         </div>
         <div className={`container ${styles.modalBody}`}>
-          <div
-            className={`justify-content-between align-items-start py-3 py-lg-4 d-lg-flex ${styles.modalContent}`}
-          >
+          <div className={`justify-content-between align-items-start py-3 py-lg-4 d-lg-flex ${styles.modalContent}`}>
             <img
               src={avatarIcon}
               alt="avatarIcon"
               className={styles.avatarIcon}
             />
-            <div className="row row-cols-2 py-3 text-nowrap row-gap-lg-3 p-lg-0  ">
-              <div className={styles.font__size}>
-                <p className="fw-bold">Nama Pasien</p>
-                <p className="mt-lg-1 ">{name}</p>
-              </div>
-              <div className={styles.font__size}>
-                <p className="fw-bold">Berat Badan</p>
-                <p className="mt-lg-1">{weight}</p>
-              </div>
-              <div className={styles.font__size}>
-                <p className="fw-bold">Jenis Kelamin</p>
-                <p className="mt-lg-1">{gender}</p>
-              </div>
-              <div className={styles.font__size}>
-                <p className="fw-bold">Tanggal Konsultasi</p>
-                <p className="mt-lg-1">{consultationDate}</p>
-              </div>
+            <div className="row row-cols-2 py-3 text-nowrap row-gap-lg-3 p-lg-0">
+              {[
+                { label: "Nama Pasien", value: name },
+                { label: "Berat Badan", value: weight },
+                { label: "Jenis Kelamin", value: gender },
+                { label: "Tanggal Konsultasi", value: consultationDate },
+              ].map((item, index) => (
+                <div key={index} className={styles.font__size}>
+                  <p className="fw-semibold ">{item.label}</p>
+                  <p className="mt-lg-1">{item.value}</p>
+                </div>
+              ))}
             </div>
           </div>
           <div
             className={`d-grid gap-2 gap-md-3 pt-lg-3 pb-5  ${styles.modalContent}`}
           >
             <div className="w-100 ">
-              <label className={`fw-bold ${styles.font__size}`}>
+              <label className={`fw-semibold  ${styles.font__size}`}>
                 Discase (Diagnosa Penyakit)
               </label>
-              <input
-                type="text"
-                placeholder="Demam"
-                className="rounded border border-secondary p-1 px-lg-2 w-100 text-capitalize  "
+              <Input 
+                className='w-100' 
+                placeHolder='Demam' 
+                name='discase'
+                // value={value}
+                // handleChange={(e) => handleChange(e) }
               />
             </div>
             <div>
-              <label className={`fw-bold ${styles.font__size}`}>Status</label>
+              <label className={`fw-semibold  ${styles.font__size}`}>Status</label>
               <Select
                 options={options}
                 name="options"
