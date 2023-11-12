9160f5e7f401857eb28b049f12f6e3d96a961080
nandorfn <firnandorfn@gmail.com>
2023-11-12 11:55:11 +0700

    fix refactor error

---
+++ src/components/ui/Cards/index.jsx
@@ -0,0 +1,11 @@
+import { ArticleCard } from "./ArticleCard";
+import { ChatList } from "./ChatList";
+import { MedicalPrescriptionCard } from "./MedicalPrescriptionCard";
+import { PatientTableRow } from "./PatientTableRow";
+
+export {
+  ArticleCard,
+  ChatList,
+  MedicalPrescriptionCard,
+  PatientTableRow
+}
