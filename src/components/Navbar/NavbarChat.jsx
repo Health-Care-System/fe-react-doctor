4ae3f0778de11aee7244045c28a30292fee24fdb
nandorfn <firnandorfn@gmail.com>
2023-11-14 18:23:20 +0700

    update: layout navbar

--- src/components/Navbar/NavbarChat.jsx
+++ src/components/Navbar/NavbarChat.jsx
@@ -5,9 +5,11 @@ import arrowDownIcon from '../../assets/icon/arrow-down-2.svg'
 import arrowLeftIcon from '../../assets/icon/arrow-left.svg'
 import { Link } from 'react-router-dom'
 import './Navbar.css'
+import { Button } from '../ui/Button'
 
 
 export const NavbarChat = () => {
+  const isOnline = true;
   const navIconMenu = [
     { icon: cameraIcon, alt: 'Camera' },
     { icon: searchIconChat, alt: 'Search' },
@@ -24,17 +26,22 @@ export const NavbarChat = () => {
           <img width={50} height={50} className="rounded-3 object-fit-cover" src="https://images.unsplash.com/photo-1568602471122-7832951cc4c5?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Profile Picture" />
           <div className="w-75 m-0">
             <h6 className=" fw-semibold">{'John Doe'}</h6>
-            <p className="line-clamp-1 m-0">{'Online'}</p>
+            <div className='d-flex align-items-center gap-1'>
+              <div className={`bullet ${isOnline ? 'bg-success-100' : 'bg-secondary-subtle'}`}></div>
+              <p className="line-clamp-1 fs-4 fw-medium m-0">{isOnline ? 'Online' : 'Offline'}</p>
+            </div>
           </div>
         </figure>
-        <figure className='d-inline-flex align-items-center gap-3'>
+        <figure className='d-inline-flex align-items-center gap-1 gap-lg-4'>
           {navIconMenu?.map((item, index) => (
+            <Button key={index}>
             <img
               key={index}
               height={'24px'}
               src={item.icon}
               alt={item.alt}
             />
+            </Button>
           ))}
         </figure>
       </nav>
