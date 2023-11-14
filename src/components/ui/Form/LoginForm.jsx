a2849dbcd6e6f4d86e994fe65c75b73c4f8899ce
Felix Winston <felixlim140@gmail.com>
2023-11-14 19:34:51 +0700

    fixed slicing

--- src/components/ui/Form/LoginForm.jsx
+++ src/components/ui/Form/LoginForm.jsx
@@ -1,30 +1,39 @@
 export const LoginForm = () => {
     
     return (
-        <form action='#' className="mt-4">
+        <form action='#' className="mt-3">
             <div className="mb-3">
-                <h3 className="fw-bold">Portal Dokter</h3>
+                <img src="../../../public/logohorizon.png" style={{ width : 200, height : 60 }}/>
+            </div>
+            <div className="mb-3">
+                <h3 className="fw-bold text-primary">Portal Dokter</h3>
                 <p>Kelola informasi kesehatan dengan aman, Silahkan masuk untuk melanjutkan</p>
             </div>
-            <div className="mb-3 d-flex flex-column gap-3">
-                <button className="btn btn-primary text-light">Sign in with Google </button>
-                <button className="btn btn-primary text-light">Sign in with Facebook</button>
-            </div>
-            <p className="text-center"> or </p>
 
             <div className="mb-3">
                 <label className="form-label">Email address</label>
-                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+                <input type="email" className="form-control" placeholder="Enter your email" id="exampleInputEmail1" aria-describedby="emailHelp"/>
             </div>
             <div className="mb-3">
                 <label className="form-label">Password</label>
-                <input type="password" className="form-control" id="exampleInputPassword1"/>
+                <input type="password" placeholder="Enter your password" className="form-control" id="exampleInputPassword1"/>
             </div>
             <div className="mb-3 form-check">
                 <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
                     <label className="form-check-label" >Remember Me</label>
             </div>
-            <button type="submit mt-2" className="btn btn-primary text-light">Submit</button>
+
+            <button type="submit" className="btn btn-primary text-light w-100">Login</button>
+
+
+            <p className="text-center mt-2"> or </p>
+
+        
+            <div className="mb-3 d-flex flex-column gap-3">
+                <button className="btn btn-outline-primary">Sign in with Google </button>
+                <button className="btn btn-outline-primary">Sign in with Facebook</button>
+            </div>
+
         </form>
     )
 }
--- src/pages/Login/LoginPage.jsx
+++ src/pages/Login/LoginPage.jsx
@@ -1,19 +1,18 @@
 import { LoginForm } from "../../components/ui/Form/LoginForm"
 
+
 const LoginPage = () => {
 
     return (
-        <div className="container mt-5">
-           <div className="row">
-                <div className="col-md-5 col-lg-6">
-
-
+        <div className="p-5">
+            <div className="row">
+                <div className="col-md-5 col-lg-6 col-6">
+                    <img width={550} height={400} src="../../../public/loginimg.png" />
                 </div>
-                <div className="col-md-5 col-lg-6">
-                    <h5>Healtify</h5>
+                <div className="col-md-5 col-lg-6 col-6">
                     <LoginForm />
                 </div>
-           </div>
+            </div>
         </div>
     )
 }
