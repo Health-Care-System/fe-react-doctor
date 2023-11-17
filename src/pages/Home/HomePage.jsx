d0b7462c3c97df9a2086e3bef80896c1aa2b2750
addinakhair55 <addi21148ti@student.nurulfikri.ac.id>
2023-11-17 19:41:00 +0700

    update: dashboard-doctor

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -3,12 +3,40 @@ import { RecentPatient } from "./components/RecentPatients";
 import { ArticleCard, UserChat } from "../../components/ui/Cards";
 import { CardContainer } from "../../components/ui/Container/CardContainer";
 import { RiwayatPasien } from "./components/RiwayatPasien";
-import doctor from '../../assets/image/doctor.png'
 import { Pasien } from "./components/Pasien";
+import { useEffect, useState } from "react";
+import axios from "axios";
 import './Home.css'
 
-
 const HomePage = () => {
+  const [userChatData, setUserChatData] = useState([]);
+  const [konsultasiData, setKonsultasiData] = useState([]);
+  const [artikelData, setArtikelData] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const [userChatResponse, konsultasiResponse, artikelResponse] = await Promise.all([
+        axios.get('/src/json/userChat-patients.json'),
+        axios.get('/src/json/konsultasi-patients.json'),
+        axios.get('/src/json/article-patients.json')
+      ]);
+
+      setUserChatData(userChatResponse.data);
+      setKonsultasiData(konsultasiResponse.data);
+      setArtikelData(artikelResponse.data);
+    } catch (error) {
+      setError('Terjadi kesalahan saat mengambil data: ' + error.message);
+    }
+  };
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
 
   return (
     <div className="p-2 w-100 home-container bg-transparent">
@@ -24,16 +52,15 @@ const HomePage = () => {
             title={'Pesan'}
             detail={'3 belum dibaca'}>
             <div className="d-flex flex-column gap-1">
-              <UserChat
-                name={'Sanjana'}
-                date={'15 Nov'}
-                message={'Bagaimana cara keren untuk meminum obat'}
-                image={doctor} />
-              <UserChat
-                name={'Sanjana'}
-                date={'15 Nov'}
-                message={'Bagaimana cara keren untuk meminum obat'}
-                image={doctor} />
+              {userChatData.map((userChat, index) => (
+                  <UserChat
+                    key={index}
+                    name={userChat.name}
+                    date={userChat.date}
+                    message={userChat.message}
+                    image={userChat.image}
+                  />
+              ))}
             </div>
           </CardContainer>
           <RiwayatPasien />
@@ -44,34 +71,28 @@ const HomePage = () => {
           className={'chat-container'}
         >
           <div className="d-flex flex-column gap-3">
-            <Pasien
-              name="Katherina Lubis"
-              gender="Laki-Laki"
-              tanggal="13 Okt 2023"
-              waktu="12.00 AM"
-              tombol="Mulai Konsultasi"
-            />
-            <Pasien
-              name="Katherina Lubis"
-              gender="Laki-Laki"
-              tanggal="13 Okt 2023"
-              waktu="12.00 AM"
-              tombol="Mulai Konsultasi"
-            />
+            {konsultasiData.map((konsultasi, index) => (
+              <Pasien
+                key={index}
+                name={konsultasi.name}
+                gender={konsultasi.gender}
+                tanggal={konsultasi.date}
+                waktu={konsultasi.time}
+                tombol="Mulai Konsultasi"
+              />
+            ))}
           </div>
         </CardContainer>
         <CardContainer className='w-auto' title={'Artikel Terbaru'} detail={'View all'}>
           <div className=" d-flex flex-column gap-4 w-100">
-            <ArticleCard
-              title="COVID-19: Sebuah Pandemi yang Menantang"
-              content="COVID-19, atau dikenal juga sebagai coronavirus disease 2019, adalah pandemi global yang telah memengaruhi hampir setiap aspek kehidupan kita."
-              date="Okt, 2023"
-            />
-            <ArticleCard
-              title="COVID-19: Sebuah Pandemi yang Menantang"
-              content="COVID-19, atau dikenal juga sebagai coronavirus disease 2019, adalah pandemi global yang telah memengaruhi hampir setiap aspek kehidupan kita."
-              date="Okt, 2023"
-            />
+            {artikelData.map((article, index) => (
+              <ArticleCard
+                key={index}
+                title={article.title}
+                content={article.content}
+                date={article.date}
+              />
+            ))}
           </div>
         </CardContainer>
       </div>
