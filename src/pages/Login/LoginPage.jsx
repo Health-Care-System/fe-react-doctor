77cb121c310d3ec183dcbefe1865b5216a814998
Felix Winston <felixlim140@gmail.com>
2023-11-18 15:49:41 +0700

    revisi

--- src/pages/Login/LoginPage.jsx
+++ src/pages/Login/LoginPage.jsx
@@ -1,15 +1,15 @@
 import { LoginForm } from "../../components/ui/Form/LoginForm"
-
+import styles from './LoginPage.module.css'
 
 const LoginPage = () => {
 
     return (
-        <div className="p-5">
+        <div className="p-5 w-full" style={styles.LoginPage}>
             <div className="row">
-                <div className="col-md-5 col-lg-6 col-6">
-                    <img width={550} height={400} src="../../../public/loginimg.png" />
+                <div className="col-md-5 col-lg-6 col-sm-6">
+                    <img width={550} height={400} src='../../../public/loginimg.png'/>
                 </div>
-                <div className="col-md-5 col-lg-6 col-6">
+                <div className="col-md-5 col-lg-6 col-sm-6" >
                     <LoginForm />
                 </div>
             </div>
