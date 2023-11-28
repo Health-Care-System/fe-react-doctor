b58d903f33b5740811054ef9784eb70666259fc6
Felix Winston <felixlim140@gmail.com>
2023-11-28 20:31:37 +0700

    added login logic

--- src/utils/Auth.js
+++ src/utils/Auth.js
@@ -1,3 +1,14 @@
+import axios from 'axios';
+
 export default class AuthController {
 
+    async login(email, password){
+        const response = await axios.post('http://34.101.122.152/login', { email, password})
+        if(response.status === 200){
+            return response.body;
+        }
+        else {
+            throw new Error('Login failed with unexpected error')
+        }
+    }
 }
