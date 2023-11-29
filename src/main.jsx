a887169c69ddf607b72b2f15a8aa34dc4fa9a0a7
nandorfn <firnandorfn@gmail.com>
2023-11-29 19:46:02 +0700

    update article

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import './scss/bootstrap.config.scss'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 const queryClient = new QueryClient()
 
@@ -16,6 +17,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <BrowserRouter>
         <App />
       </BrowserRouter>
+      <ReactQueryDevtools />
     </QueryClientProvider>
   </React.StrictMode>,
 )
