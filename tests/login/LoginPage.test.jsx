061ad1cc46258eb9444c76b98b35e4fa416dd93a
Felix Winston <felixlim140@gmail.com>
2023-12-04 20:59:04 +0700

    add tests

--- tests/login/LoginPage.test.jsx
+++ tests/login/LoginPage.test.jsx
@@ -1,13 +1,27 @@
-import { render } from '@testing-library/react';
-import { describe, vitest } from 'vitest';
+import { render, screen, waitFor, expect, fireEvent } from '@testing-library/react'
+import { describe, test } from 'vitest'
+import { LoginPage } from '../src/pages/Login/LoginPage'
 
 const renderLogin = () => {
     render(
-        <LoginPage/>
+        <LoginPage />
     )
 }
 
+describe('test login page', () => {
 
-describe('login page should display login', () => {
+    test('should display username and password', async () => {
+        renderLogin()
 
+        const submit = screen.getByText('Masuk')
+        fireEvent.click(submit)
+
+        await waitFor(() => {
+            const emailError = screen.getByText('Email wajib diisi!')
+            const passwordError = screen.getByText('Password wajib diisi!')
+
+            expect(emailError).toBeInTheDocument();
+            expect(passwordError).toBeInTheDocument();
+        })
+    })
 })
