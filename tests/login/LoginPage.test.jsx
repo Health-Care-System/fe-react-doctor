25a88d05fdc1cf7f9d2483730b2e66c003a9dba5
Felix Winston <felixlim140@gmail.com>
2023-12-04 20:22:41 +0700

    Merge pull request #77 from Health-Care-System/dev
    Dev

---
+++ tests/login/LoginPage.test.jsx
@@ -0,0 +1,13 @@
+import { render } from '@testing-library/react';
+import { describe, vitest } from 'vitest';
+
+const renderLogin = () => {
+    render(
+        <LoginPage/>
+    )
+}
+
+
+describe('login page should display login', () => {
+
+})
