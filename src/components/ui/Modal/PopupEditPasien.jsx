fa8f50913043ee52e81b2b8eac519f7454a547c5
addinakhair55 <addi21148ti@student.nurulfikri.ac.id>
2023-12-11 22:24:28 +0700

    add: forget-password

---
+++ src/components/ui/Modal/PopupEditPasien.jsx
@@ -0,0 +1,108 @@
+/* 
+  Note:
+  1. Terdapat props yang akan dilempar yaitu patiendtData dan juga closeModal
+  2. Cara pemanggilan component ini dapat dilakukan dengan cara seperti dibawah ini:
+  const [openModal, setOpenModal] = useState(false)
+
+  const patientData = {
+    name: 'Joshua Kristin',
+    weight: '58 Kg',
+    gender: 'Laki-laki',
+    consultationDate: '17 Oktober 2023',
+  };
+
+  return (
+      <Button onClick={() => setOpenModal(true)}>Open Modal</Button>
+      {openModal && <PopupEditPasien closeModal={setOpenModal} patientData={patientData}/>}
+    )
+*/
+
+import closeMenu from "../../../assets/icon/close.svg";
+import { Button } from "../Button";
+import styles from "./Modal.module.css";
+import avatarIcon from "../../../assets/icon/avatar.svg";
+import { Input, Select } from "../Form";
+
+export const PopupEditPasien = ({ closeModal, patientData }) => {
+  const { name, weight, gender, consultationDate } = patientData;
+  const options = [
+    { label: "Pilihan 1", value: "option1" },
+    { label: "Pilihan 2", value: "option2" },
+  ];
+
+  return (
+    <section className={styles.modalBackground}>
+      <div className={`mx-2 ${styles.modalContainer}`}>
+        <div className="d-flex justify-content-between align-items-center border-bottom pb-2 pb-lg-3 container-fluid ">
+          <h5 className={styles.font__size}>Edit</h5>
+          <Button className="btn p-0 m-0">
+            <img
+              src={closeMenu}
+              alt="closeMenu"
+              className="w-75"
+              onClick={() => closeModal(false)}
+            />
+          </Button>
+        </div>
+        <div className={`container ${styles.modalBody}`}>
+          <div className={`justify-content-between align-items-start py-3 py-lg-4 d-lg-flex ${styles.modalContent}`}>
+            <img
+              src={avatarIcon}
+              alt="avatarIcon"
+              className={styles.avatarIcon}
+            />
+            <div className="row row-cols-2 py-3 row-gap-2  text-nowrap row-gap-lg-3 p-lg-0">
+              {[
+                { label: "Nama Pasien", value: name },
+                { label: "Berat Badan", value: weight },
+                { label: "Jenis Kelamin", value: gender },
+                { label: "Tanggal Konsultasi", value: consultationDate },
+              ].map((item, index) => (
+                <div key={index} className={styles.font__size}>
+                  <p className="fw-semibold ">{item.label}</p>
+                  <p className="mt-lg-1">{item.value}</p>
+                </div>
+              ))}
+            </div>
+          </div>
+          <div
+            className={`d-grid gap-2 gap-md-3 pt-lg-3 pb-5  ${styles.modalContent}`}
+          >
+            <div className="w-100 ">
+              <label className={`fw-semibold  ${styles.font__size}`}>
+                Discase (Diagnosa Penyakit)
+              </label>
+              <Input 
+                className='w-100' 
+                placeHolder='Demam' 
+                name='discase'
+                // value={value}
+                // handleChange={(e) => handleChange(e) }
+              />
+            </div>
+            <div>
+              <label className={`fw-semibold  ${styles.font__size}`}>Status</label>
+              <Select
+                options={options}
+                name="options"
+                // handleChange={(e) => handleChange(e) }
+                // value={value}
+              />
+            </div>
+          </div>
+        </div>
+        <div className={`ps-2 ${styles.modalFooter} d-flex gap-3`}>
+          <Button className={`btn-primary text-white ${styles.font__size}`}>
+            Save
+          </Button>
+          <Button
+            className={`btn-danger ${styles.font__size}`}
+            onClick={() => closeModal(false)}
+          >
+            Cancel
+          </Button>
+        </div>
+      </div>
+    </section>
+  );
+};
--- src/pages/ForgotPassword/ForgotPasswordPage.jsx
+++ src/pages/ForgotPassword/ForgotPasswordPage.jsx
@@ -4,19 +4,20 @@ import { Link } from "react-router-dom";
 // Components
 import { Input } from '../../components/ui/Form';
 import { Button } from '../../components/ui/Button';
-import { ErrorMsg } from "../../components/Error/ErrorMsg";
+// import { ErrorMsg } from "../../components/Error/ErrorMsg";
 
 // Assets
+import visibility from '../../assets/icon/visibility.svg'
 import brandLogo from '../../assets/icon/brandLogo.png'
 import doctorLogin from '../../assets/image/doctorLogin.png'
-
+import { useState } from "react";
 
 export const ForgotPasswordPage = () => {
     return (
-        <div className=" min-vh-100 px-5">
+        <div className="min-vh-100 px-5">
             <div className="d-flex min-vh-100 flex-column flex-md-row justify-content-evenly align-items-center">
                 <div className="d-none d-lg-block">
-                    <img width={528} height={591} src={doctorLogin} />
+                    <img width={528} height={591} src={doctorLogin} alt="Doctor Login" />
                 </div>
                 <div style={{ maxWidth: '29.9375rem' }}>
                     <LoginForm />
@@ -27,54 +28,168 @@ export const ForgotPasswordPage = () => {
 }
 
 const LoginForm = () => {
+    const [step, setStep] = useState(1);
+    const [showPassword, setShowPassword] = useState(false);
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+
+    const handleResetPassword = (e) => {
+        e.preventDefault();
+        setStep(2);
+    };
+
+    const handleVerifyOTP = (e) => {
+        e.preventDefault();
+        setStep(3);
+    };
+
+    const handleCreateNewPassword = (e) => {
+        e.preventDefault();
+    };
+
+    const togglePasswordVisibility = () => {
+        setShowPassword((prevShowPassword) => !prevShowPassword);
+    };
+
+    const InputProps = {
+        type: showPassword ? "text" : "password",
+        className: "form-control-lg bg-neutral-300 border-0 form-color",
+        placeholder: "Masukkan password baru",
+        onChange: (e) => setPassword(e.target.value)
+    };
+
     return (
         <>
             <div className="d-flex justify-content-center">
-                <img
-                    src={brandLogo}
-                    style={{
-                        width: 318,
-                        height: 92
-                    }} />
+                <img src={brandLogo} style={{ width: 318, height: 92 }} alt="Brand Logo" />
             </div>
-            <form disabled={true} className="mt-3">
+            <form disabled={step === 2 || step === 3} className="mt-3" onSubmit={step === 1 ? handleResetPassword : step === 2 ? handleVerifyOTP : handleCreateNewPassword}>
                 <div className="mb-3">
-                    <h3 className="fw-bold text-primary">Lupa Password?</h3>
-                    <p>Jangan khawatir, kami akan mengirimkan Anda instruksi reset password.</p>
+                    {step === 1 && (
+                        <>
+                            <h2 className="fw-bold text-primary text-center">Lupa Password?</h2>
+                            <p className="text-center">Masukkan alamat email yang terkait dengan akun Anda</p>
+                        </>
+                    )}
+                    {step === 2 && (
+                        <h2 className="fw-bold text-primary text-center">Verifikasi OTP</h2>
+                    )}
+                    {step === 3 && (
+                        <>
+                            <h2 className="fw-bold text-primary text-center">Masukkan Password Baru</h2>
+                            <p className="text-center">Kata sandi baru Anda harus berbeda dengan kata sandi yang digunakan sebelumnya</p>
+                        </>
+                    )}
                 </div>
 
-                <div className="mb-3">
-                    <label
-                        htmlFor='emailLogin'
-                        className="form-label fw-medium">
-                        Email
-                    </label>
-                    <Input
-                        disabled={true}
-                        type="email"
-                        className="form-control-lg bg-neutral-300 border-0 form-color"
-                        name="email"
-                        placeHolder="Masukkan email anda"
-                    />
-                    <ErrorMsg msg={'Mohon maaf, saat ini fitur reset password sedang tidak tersedia karena sedang dalam pemeliharaan. Silakan hubungi Customer Service kami untuk bantuan lebih lanjut terkait reset password.'} />
-                </div>
+                {step === 1 && (
+                    <div className="mb-3">
+                        <label htmlFor='emailLogin' className="form-label fw-medium">
+                            Email
+                        </label>
+                        <Input
+                            type="email"
+                            {...InputProps}
+                            name="email"
+                            placeholder="Masukkan email anda"
+                        />
+                    </div>
+                )}
 
-                <div className="d-flex justify-content-end mb-3">
-                    <Link
-                        to={'/login'}
-                        className="p-2 g-col-6 text-black link-underline-dark">
-                        Kembali ke halaman login?
-                    </Link>
-                </div>
+                {step === 2 && (
+                    <div className="mb-3">
+                        <label htmlFor="otpInput" className="form-label fw-medium text-center">
+                            Masukkan 4 kode OTP yang telah dikirimkan ke email Anda.
+                        </label>
+                        <div className="d-flex">
+                            {[1, 2, 3, 4].map((index) => (
+                                <Input
+                                    key={index}
+                                    type="text"
+                                    className="form-control-lg bg-neutral-300 border-0 form-color w-25 m-2 text-center"
+                                    name={`otpInput${index}`}
+                                    placeholder="0"
+                                    maxLength={1}
+                                    inputMode="numeric"
+                                    onChange={(e) => {
+                                        if (e.target.value.length === e.target.maxLength) {
+                                            const nextInput = document.querySelector(`[name=otpInput${index + 1}]`);
+                                            if (nextInput) {
+                                                nextInput.focus();
+                                            }
+                                        }
+                                    }}
+                                />
+                            ))}
+                        </div>
+                    </div>
+                )}
+
+                {step === 3 && (
+                    <>
+                        <div className="mb-3">
+                            <label htmlFor='newPassword' className="form-label fw-medium">
+                                Password
+                            </label>
+                            <div className="input-group">
+                                <Input
+                                    {...InputProps}
+                                    name="newPassword"
+                                />
+                                <span
+                                    className="input-group-text border-0 bg-neutral-300 cursor-pointer"
+                                    onClick={togglePasswordVisibility}
+                                >
+                                    <img
+                                        src={visibility}
+                                        alt={showPassword ? 'hide' : 'show'}
+                                    />
+                                </span>
+                            </div>
+                        </div>
+                        <div className="mb-3">
+                            <label htmlFor='confirmPassword' className="form-label fw-medium">
+                                Konfirmasi Password
+                            </label>
+                            <div className="input-group">
+                                <Input
+                                    {...InputProps}
+                                    name="confirmPassword"
+                                    placeholder="Konfirmasi password baru"
+                                    onChange={(e) => setConfirmPassword(e.target.value)}
+                                />
+                                <span
+                                    className="input-group-text border-0 bg-neutral-300 cursor-pointer"
+                                    onClick={togglePasswordVisibility}
+                                >
+                                    <img
+                                        src={visibility}
+                                        alt={showPassword ? 'hide' : 'show'}
+                                    />
+                                </span>
+                            </div>
+                        </div>
+                    </>
+                )}
+
+                {step !== 1 && (
+                    <div className="d-flex justify-content-end mb-3">
+                        <Link to={'/login'} className="p-2 g-col-6 text-black link-underline-dark">
+                            Kembali ke halaman login?
+                        </Link>
+                    </div>
+                )}
 
                 <Button
                     type="submit"
                     className="btn-primary text-light w-100"
-                    disabled={true}
+                    disabled={step === 3}
                 >
-                    Reset Password
+                    {step === 1 ? 'Reset Password' : step === 2 ? 'Verifikasi OTP' : 'Pulihkan Password'}
                 </Button>
             </form>
         </>
     )
 }
+
+export default LoginForm;
