cd6086e48dc079eeb54d1118773aa78bf6ee7424
nandorfn <firnandorfn@gmail.com>
2023-12-11 22:26:56 +0700

    up toggle

--- src/store/store.jsx
+++ src/store/store.jsx
@@ -1,8 +1,18 @@
 import { create } from "zustand";
+import client from "../utils/auth";
+import { toast } from "react-toastify";
 
 // Global state untuk fitur aktif dan nonaktif di Navbar
 export const useStatus = create((set) => ({
   isActive: false,
+  fetch: async () => {
+    try {
+      const res = await client.get('/doctors/status');
+      set({ isActive: res?.data?.results?.status ?? false });
+    } catch (error) {
+      toast.error('Gagal mengambil status dokter, harap muat ulang halaman!', { delay: 800});
+    }
+  },
   handleStatus: () => {
     set((state) => ({
       isActive: !state.isActive
