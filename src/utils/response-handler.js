af733ef0ea268a867e0abc052e945e5127e432f7
nandorfn <firnandorfn@gmail.com>
2023-12-15 20:42:21 +0700

    add: forgot pass

--- src/utils/response-handler.js
+++ src/utils/response-handler.js
@@ -24,3 +24,66 @@ export const handleLoginError = (error, setErrors) => {
     }
   }
 };
+export const handleEmailForgotPasswordError = (error, setErrors) => {
+  if (error.response) {
+    const field = error?.response?.data?.meta?.message;
+    switch (field) {
+      case 'failed to get send OTP':
+        setErrors({
+          email: 'Gagal mengirim verifikasi OTP, harap masukan email yang valid'
+        });
+        break;
+      case "Field 'Email' failed on 'email' validation":
+        setErrors({
+          email: 'Masukan email yang valid!',
+        });
+        break;
+      default:
+        setErrors({
+          email: 'Maaf, permintaan anda tidak dapat kami proses saat ini. Harap coba lagi',
+        });
+    }
+  }
+};
+export const handleVerifyOTPError = (error, setErrors) => {
+  if (error.response) {
+    const field = error?.response?.data?.meta?.message;
+    switch (field) {
+      case 'failed to get OTP not found':
+        setErrors({
+          otpError: 'OTP yang anda inputkan tidak valid'
+        });
+        break;
+      default:
+        setErrors({
+          email: 'Maaf, permintaan anda tidak dapat kami proses saat ini. Harap coba lagi',
+        });
+    }
+  }
+};
+export const handleResetPasswordError = (error, setErrors) => {
+  if (error.response) {
+    const field = error?.response?.data?.meta?.message;
+    switch (field) {
+      case 'Invalid request':
+        setErrors({
+          confirmPassword: 'Input form tidak valid'
+        });
+        break;
+      case 'failed to get OTP verification failed':
+        setErrors({
+          confirmPassword: 'Gagal memverifikasi kode OTP, harap coba lagi!'
+        });
+        break;
+      case 'failed to get update password':
+        setErrors({
+          confirmPassword: 'Gagal untuk memperbarui pasword baru, harap coba lagi beberapa saat lagi!'
+        });
+        break;
+      default:
+        setErrors({
+          email: 'Maaf, permintaan anda tidak dapat kami proses saat ini. Harap coba lagi',
+        });
+    }
+  }
+};
